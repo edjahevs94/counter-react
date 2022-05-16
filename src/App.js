132fe3e1f16686625f14461f711ccd2f373c52e9
Edgard <edjahevs@gmail.com>
2022-05-15 22:06:47 -0500

    creating conter using react state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0)
+   
+  function add() {
+      //usando funciones tradicionales
+      setCount(function(prevCount){
+              return prevCount + 1
+          } )
+  }
+  
+  
+  function subtract() {
+      //usando arrow functions
+      setCount(prevCount => prevCount - 1)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="counter">
+            <button className="counter--minus" onClick={subtract}>–</button>
+            <div className="counter--count">
+                <h1>{count}</h1>
+            </div>
+            <button className="counter--plus" onClick={add}>+</button>
+        </div>
   );
 }
 
