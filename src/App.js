553b53734e83735c87bd0a23383f22458fc56422
Edgard <20133252@ealoe.ulima.edu.pe>
2022-09-22 12:41:43 -0500

    adding empty exercise version

--- src/App.js
+++ src/App.js
@@ -3,27 +3,16 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = React.useState(0)
-   
-  function add() {
-      
-      setCount(count + 1)
-  }
-  
-  
-  function subtract() {
-      
-      setCount(count - 1)
-  }
+ 
 
 
   return (
     <div className="counter">
-            <button className="counter--minus" onClick={subtract}>–</button>
+            <button className="counter--minus">–</button>
             <div className="counter--count">
                 <h1>{count}</h1>
             </div>
-            <button className="counter--plus" onClick={add}>+</button>
+            <button className="counter--plus">+</button>
         </div>
   );
 }
